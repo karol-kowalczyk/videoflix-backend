150d7464264df97f49c0bc90a5abbbef562f0935
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-26 19:41:07 +0100

    Added post_delete signal to remove video file on deletion

--- videoflix_app/signals.py
+++ videoflix_app/signals.py
@@ -1,10 +1,17 @@
 from .models import Video
 from django.dispatch import receiver
-from django.db.models.signals import post_save
+from django.db.models.signals import post_save, post_delete
+import os
 
 @receiver(post_save, sender=Video)
-def video_post_safe(sender, instance, created, **kwargs):
-        print('Video save')
-        if created:
-                print('New video created')
+def video_post_save(sender, instance, created, **kwargs):
+    print('Video save')
+    if created:
+        print('New video created')
 
+@receiver(post_delete, sender=Video)
+def auto_delete_file_on_delete(sender, instance, **kwargs):
+    if instance.video_file:
+        file_path = instance.video_file.path
+        if os.path.isfile(file_path):
+            os.remove(file_path)
