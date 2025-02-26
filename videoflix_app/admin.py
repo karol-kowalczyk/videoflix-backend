46cbdd5a257ac2d1b6efdaad5c222158a2b0bbea
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-26 19:16:49 +0100

    fixed my admin.py. Now every video is added to my media folder

--- videoflix_app/admin.py
+++ videoflix_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Video
 
-# Register your models here.
+@admin.register(Video)
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at')  # Optional: Felder für die Admin-Ansicht
