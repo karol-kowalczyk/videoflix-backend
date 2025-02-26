f6d20af930e7c6493c777f789bfbf46d03057ef8
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-02-26 20:22:43 +0100

    Fix KeyError in convert_480p function by using f-strings for formatting

---
+++ videoflix_app/tasks.py
@@ -0,0 +1,6 @@
+import subprocess
+
+def convert_480p(source):
+    target = f"{source}_480p.mp4"
+    cmd = f'ffmpeg -i "{source}" -s hd480 -c:v libx264 -crf 23 -c:a aac -strict -2 "{target}"'
+    subprocess.run(cmd, shell=True)
