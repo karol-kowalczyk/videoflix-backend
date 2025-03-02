cf6012e8127ef9c93dbeac8f73d7f93f45d26899
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-03-02 14:34:04 +0100

    custom user created

---
+++ users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth import AbstractUser
+
+# Create your models here.
+class CustomUser(AbstractUser):
+        custom = models.CharField(max_length=500, default='')
+        phone = models.CharField(max_length=20, default='')
+        address = models.CharField(max_length=150, default='')
