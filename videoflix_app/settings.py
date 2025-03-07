912691c6a354c1b81556e3c4943ab4f0c61100d1
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-03-07 18:19:09 +0100

    added redis

--- videoflix_app/settings.py
+++ videoflix_app/settings.py
@@ -29,10 +29,22 @@ DEBUG = True
 ALLOWED_HOSTS = ['http://127.0.0.1:5500', '127.0.0.1']
 
 CORS_ALLOWED_ORIGINS = [
-   "http://localhost:4200",      # Standard Angular-Port
-   "http://localhost:57639",     # Dein aktueller Port
-   "http://127.0.0.1:8000",
+    "http://localhost:4200",      # Standard Angular-Port
+    "http://localhost:57639",     # Dein aktueller Port
+    "http://127.0.0.1:8000",
 ]
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient"
+        },
+        "KEY_PREFIX": "videoflix"
+    }
+}
+
 
 # Application definition
 
