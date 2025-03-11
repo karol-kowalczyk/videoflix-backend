89c5006f73976b6f3cd65579bb13ae6d1a0d0f36
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-03-11 14:42:49 +0100

    css not loaded

--- content/urls.py
+++ content/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import VideoViewSet
+from django.conf.urls.static import static
+from videoflix_app import settings
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+
 
 router = DefaultRouter()
 router.register(r'videos', VideoViewSet)
