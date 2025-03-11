dbf5d89cd1c277908df110c3a6e3d6cc3d3edcf7
Karol Kowalczyk <KarolKowalczykPL@Gmail.com>
2025-03-11 12:45:14 +0100

    added my server to the 'allowed hosts' section

--- videoflix_app/settings.py
+++ videoflix_app/settings.py
@@ -26,13 +26,14 @@ SECRET_KEY = 'django-insecure-#d91((1kh9lzw)%68r6+4x^24^3_rc#m2(5+9%^@kjl2b)^t*g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['http://127.0.0.1:5500', '127.0.0.1', 'localhost:8000', 'localhost']
+ALLOWED_HOSTS = ['http://127.0.0.1:5500', '127.0.0.1', 'localhost:8000', 'localhost', '34.13.144.92']
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:4200",      # Standard Angular-Port
     "http://localhost:57639",     # Dein aktueller Port
     "http://127.0.0.1:8000",
     "https://videoflix.karol-kowalczyk.de",
+    '34.13.144.92'
 ]
 
 CACHES = {
